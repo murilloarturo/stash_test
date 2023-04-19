db613939b3d933db7e05fcb0fa9e1361d3adcf53
Arturo <amurillop90@gmail.com>
2023-04-19 20:15:24 +0200

    Initial commit

---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by Arturo Murillo on 19/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
