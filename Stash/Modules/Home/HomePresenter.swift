d057812bf98790bc632ac7d5cffa7945739992a5
Arturo <amurillop90@gmail.com>
2023-04-19 22:31:04 +0200

    Add HomePresenter

---
+++ Stash/Modules/Home/HomePresenter.swift
@@ -0,0 +1,18 @@
+//
+//  HomePresenter.swift
+//  Stash
+//
+//  Created by Arturo Murillo on 19/4/23.
+//
+
+import Foundation
+
+final class HomePresenter {
+    private let name: String
+    private let host: String
+    
+    init(name: String, host: String) {
+        self.name = name
+        self.host = host
+    }
+}
