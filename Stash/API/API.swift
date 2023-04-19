d0e258be44ab5e77e055e83f190daa23768ba6fd
murilloarturo <amurillop90@gmail.com>
2023-04-19 21:18:15 +0200

    Add API class (#1)

---
+++ Stash/API/API.swift
@@ -0,0 +1,18 @@
+//
+//  API.swift
+//  Stash
+//
+//  Created by Arturo Murillo on 19/4/23.
+//
+
+import Foundation
+
+final class API {
+    private let name: String
+    private let path: String
+    
+    init(name: String, path: String) {
+        self.name = name
+        self.path = path
+    }
+}
